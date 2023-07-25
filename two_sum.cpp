f06fc750d5ecb6fc4d5ea83bbdb110fb54fbeb50
AVI MONDAL <avimondal@AVIs-MacBook-Air.local>
2023-07-25 23:37:59 +0600

    Leetcode

---
+++ two_sum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+//#include<conio.h>
+using namespace std;
+int main()
+{
+	int n,nums[10],targrt = 9;
+	int i;
+	for(i=1; i<=n; i++){
+		cin >> nums[i];
+
+		if(nums[i] + nums[i] == targrt){
+			cout >> nums[i];
+		}
+	}
+
+	//getch();
+}
